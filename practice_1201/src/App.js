ae2b1ca5e54381dae2215adfd0e868ce530d7b7d
LauraIu <iuga.laura.katalin.com>
2021-12-02 17:43:34 +0100

    .

--- practice_1201/src/App.js
+++ practice_1201/src/App.js
@@ -1,33 +1,58 @@
 import './App.css';
+import { useState } from 'react';
 
-function App () {
-
-  function ZeroBeforeNumbers(num) {      
+function zeroBeforeNumbers (num) {
+    
     let result = [];
 
     if (num < 1) {
         return
     }
     
-    const numLength = num.toString().length;
-    const nulls = "0".repeat(numLength);
+    let numLength = num.toString().length;
+    //console.log(numLength);
 
-    for(let i = 1; i <= num; i++) {
-      let strNumbersItem = i.toString();
-      if (strNumbersItem.length < numLength) {
-        strNumbersItem = nulls.concat(strNumbersItem).substr(-numLength, numLength);
-      }   
-      result[i - 1] = strNumbersItem;
+    let nulls = "0".repeat(numLength);
+    //console.log(nulls);
+
+    for(let i=1; i <= num; i++) 
+    {
+        let strNumbersItem = i.toString();
+        if (strNumbersItem.length < numLength)
+        {
+            strNumbersItem = nulls.concat(strNumbersItem).substr(-numLength, numLength);
+        }   
+       result[i-1] = strNumbersItem;
     }
     return result;
-  }
-
-  return (
-    <div className="app-container">hello
-      <ZeroBeforeNumbers />      
-    </div>
-  )
 }
+
+
+
+function App () {
+    
+    const [val, setVal] = useState ("");
+    const [numList, setNumList] = useState([]);   
+
+    const clickHandler =() => {
+        let result = zeroBeforeNumbers (val);
+        setNumList (result)
+    }
+
+    return (
+        <div className="app-conatiner">
+            <div className="container">                
+                <input type="number" placeholder="Enter your number" value={val} onChange={(e) => setVal(e.target.value)} />
+                <button onClick={ clickHandler } >Submit</button>                              
+            </div>
+                <p>{ numList.map(numItem => (
+                    <p>{ numItem }</p>)
+                )}
+                </p>
+        </div>
+    );
+  
+};
 
 export default App;
 
